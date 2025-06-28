3099255d38350e70c9f190d69b888f7e1fa8e053
luciayin9944 <luxiayin94@gmail.com>
2025-06-28 01:34:29 -0700

    update server

--- server/app.py
+++ server/app.py
@@ -8,21 +8,99 @@ from models import User, Expense, UserSchema, ExpenseSchema
 from flask_jwt_extended import create_access_token, get_jwt_identity, verify_jwt_in_request
 
 
+@app.before_request
+def check_if_logged_in():                 
+    open_access_list = ['signup', 'login']
+    
+    if request.endpoint not in open_access_list:
+        try:
+            verify_jwt_in_request()
+        except:
+            return {'error': '401 Unauthorized'}, 401
+
 
 class Signup(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+
+        user = User(username=username)
+        user.password_hash = password
+
+        try:
+            db.session.add(user)
+            db.session.commit()
+            access_token = create_access_token(identity=str(user.id))
+            response = make_response(jsonify(token=access_token, user=UserSchema().dump(user)), 200)
+            return response
+        except IntegrityError:
+            return {'errors': ['422 Unprocessable Entity']}, 422
+
+
 
 class WhoAmI(Resource):
-    pass
+    def get(self):
+        user_id = get_jwt_identity()
+        user = User.query.filter(User.id==user_id).first()
+
+        return UserSchema().dump(user), 200
+    
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
 
-class Logout(Resource):
-    pass
+        user = User.query.filter(User.username==username).first()
 
-class ExpenseIndex(Resource):
-    pass
+        if user and user.authenticate(password):
+            access_token = create_access_token(identity=str(user.id))
+            response = make_response(jsonify(token=access_token, user=UserSchema().dump(user)), 200)
+            return response
+        
+        return {'errors': ['401 Unauthorized']}, 401
+
+# class Logout(Resource):
+#     pass
+
+
+
+class Expense(Resource):
+    def get(self):
+        # expenses = [ExpenseSchema().dump(e) for e in Expense.query.all()]
+        # return expenses, 200
+
+        #pagination
+        page = request.args.get("page", 1, type=int)
+        per_page = request.args.get("per_page", 5, type=int)
+        pagination = Expense.query.paginate(page=page, per_page=per_page, error_out=False)
+        expenses = pagination.items
+
+        return {
+            "expense_records_page": page,
+            "expenses": [ExpenseSchema().dump(e) for e in expenses]
+        }
+
+    
+
+    def post(self):
+        data = request.get_json()
+
+        expense = Expense(
+            purchase_item = data.get('purchase_item'),
+            amount=data.get('amount'),
+            date = data.get('date'),
+            user_id = get_jwt_identity()
+        )
+
+        try:
+            db.session.add(expense)
+            db.session.commit()
+            return ExpenseSchema().dump(expense), 201
+        except IntegrityError:
+            return {'errors': ['422 Unprocessable Entity']}, 422
 
 
 
@@ -31,8 +109,8 @@ class ExpenseIndex(Resource):
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(WhoAmI, '/me', endpoint='me')
 api.add_resource(Login, '/login', endpoint='login')
-api.add_resource(Logout, '/logout', endpoint='logout')
-api.add_resource(ExpenseIndex, '/expenses', endpoint='expenses')
+#api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(Expense, '/expenses', endpoint='expenses')
 
 
 if __name__ == '__main__':
